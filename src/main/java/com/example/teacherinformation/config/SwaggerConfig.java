1e02ead862d60b7e929ca11b24424d27d78185a4
diyastursunkali <dias.tursunkali@gmail.com>
2021-12-07 07:44:23 +0600

    Endterm commit

--- src/main/java/com/example/teacherinformation/config/SwaggerConfig.java
+++ src/main/java/com/example/teacherinformation/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.teacherinformation.configuration;
+package com.example.teacherinformation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
