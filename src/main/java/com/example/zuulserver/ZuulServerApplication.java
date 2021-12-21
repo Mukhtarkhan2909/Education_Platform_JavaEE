88ff62ca9664b2368b54cd8626565796816e12e2
Mukhtarkhan2909 <m.nalibayev2900@gmail.com>
2021-12-21 06:57:45 +0600

    Final Exam commit

--- src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.example.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient    // It acts as a eureka client
@@ -14,4 +18,13 @@ public class ZuulServerApplication {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+        return new RestTemplate(requestFactory);
+    }
 }
